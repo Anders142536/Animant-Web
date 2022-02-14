b4a2c0735483f849c3ad5ca3bb361b340aac9b17
Anders <dom.galbavy@gmail.com>
2022-02-14 02:09:23 +0100

    navigation from and to settings menu works now

---
+++ src/SettingsEditor.svelte
@@ -0,0 +1,23 @@
+<style>
+
+
+</style>
+
+<script>
+	import { currentView } from './stores'
+	import { icons, views } from './enums'
+
+	function done() {
+		console.log("done editing settings")
+		currentView.set(views.PlayerList)
+	}
+
+</script>
+
+<div class="button-bar">
+	<div />
+	<button on:click={done}>
+		Done
+		<i class={icons.chevronright}></i>
+	</button>
+</div>
